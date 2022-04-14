6e7853832754019fe4b8c188547e4ba1551df78f
FabGonzalezV <programingfyou@gmail.com>
2022-04-14 00:34:52 -0500

    cambios

--- simulatedAnnealing/js/index.js
+++ simulatedAnnealing/js/index.js
@@ -1,26 +1,55 @@
 window.onload = function () {
     let frame = document.getElementById('frame');
     let ctx = frame.getContext('2d');
-
+    let arrayCities;
+    let run;
     let set = document.getElementById('set-cities');
+
     set.addEventListener('click', () => {
         let nCities = document.getElementById('n-cities').value;
-        if (nCities) { CreateNodes(nCities) }
-
-
+        if (nCities) {
+            arrayCities = CreateNodes(nCities);
+        }
+        else alert("valor invalido");
     });
 
+    run = document.getElementById('btn-run');
+    run.addEventListener('click', () => {
+        paintCities(arrayCities, ctx);
+    });
+
+}
+
+function paintCities(arrayCities, ctx) {
+    let r = 5;
+
+    for (let i = 0; i < arrayCities.length - 1; i++) {
+
+        for (let j = 0; j < arrayCities.length; j++) {
+            console.log(arrayCities[j].X, arrayCities[j].Y)
+            ctx.fillStyle = "blue";
+            ctx.arc(arrayCities[j].X, arrayCities[j].Y, r, 2, 2 * Math.PI);
+            ctx.fill();
+        }
+
+    }
 
 }
 
 function CreateNodes(nCities) {
 
     let Coordinates = { X: 0, Y: 0 };
+
     let Nodes = new Array();
     const Max = 301;
     const Min = 1;
     let x, y = 0;
-    for (let i = 0; i < 1000; i++) {
+
+    for (let i = 0; i < nCities; i++) {
+        Nodes.push(Coordinates);
+    }
+
+    for (let i = 0; i < nCities; i++) {
         x = Math.random() * (Max - Min) + Min
         y = Math.random() * (Max - Min) + Min
 
@@ -31,7 +60,8 @@ function CreateNodes(nCities) {
 
         }
     }
-      
+
 Nodes.forEach(nodo =>{console.log(nodo.X, nodo.Y)})
 
+    return Nodes;
 }
