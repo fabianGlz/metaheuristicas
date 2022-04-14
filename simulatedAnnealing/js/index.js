3f8f5ff3bdc979e035907f3117ecad268bbacee3
FabGonzalezV <programingfyou@gmail.com>
2022-04-14 02:22:56 -0500

    cambios

--- simulatedAnnealing/js/index.js
+++ simulatedAnnealing/js/index.js
@@ -3,7 +3,15 @@ window.onload = function () {
     let ctx = frame.getContext('2d');
     let arrayCities;
     let run;
+    let wFrame = 600;
+    let hFrame = 550;
+    let xFrame = 0;
+    let yFrame = 0;
     let set = document.getElementById('set-cities');
+
+    frame.setAttribute("width", wFrame);
+    frame.setAttribute("height", hFrame);
+    cleanScreen(ctx, xFrame, yFrame, wFrame, hFrame);
 
     set.addEventListener('click', () => {
         let nCities = document.getElementById('n-cities').value;
@@ -12,24 +20,39 @@ window.onload = function () {
         }
         else alert("valor invalido");
     });
-
     run = document.getElementById('btn-run');
     run.addEventListener('click', () => {
-        paintCities(arrayCities, ctx);
+        paintCities(arrayCities, ctx, xFrame, yFrame, wFrame, hFrame);
     });
-
 }
 
-function paintCities(arrayCities, ctx) {
+function cleanScreen(ctx, xFrame, yFrame, wFrame, hFrame) {
+    ctx.fillStyle = "black";
+    ctx.rect(xFrame, yFrame, wFrame, hFrame);
+    ctx.fill();
+}
+
+function paintCities(arrayCities, ctx, xFrame, yFrame, wFrame, hFrame) {
     let r = 5;
+    cleanScreen(ctx, xFrame, yFrame, wFrame, hFrame);
 
-    for (let i = 0; i < arrayCities.length - 1; i++) {
+    for (let i = 0; i < arrayCities.length; i++) {
+        ctx.beginPath()
+        ctx.fillStyle = "blue";
+        ctx.arc(arrayCities[i].X, arrayCities[i].Y, r, 0, 2 * Math.PI);
+        
+        ctx.closePath();
+        ctx.fill();
+        for (let j = 1; j < arrayCities.length-1; j++) {
+            ctx.beginPath()
+            ctx.strokeStyle = "green";
+             
+            ctx.moveTo(arrayCities[i].X, arrayCities[i].Y);
+            ctx.lineTo(arrayCities[j].X, arrayCities[j].Y);
+            ctx.closePath();
+             
+            ctx.stroke();
 
-        for (let j = 0; j < arrayCities.length; j++) {
-            console.log(arrayCities[j].X, arrayCities[j].Y)
-            ctx.fillStyle = "blue";
-            ctx.arc(arrayCities[j].X, arrayCities[j].Y, r, 2, 2 * Math.PI);
-            ctx.fill();
         }
 
     }
@@ -38,30 +61,20 @@ function paintCities(arrayCities, ctx) {
 
 function CreateNodes(nCities) {
 
-    let Coordinates = { X: 0, Y: 0 };
-
-    let Nodes = new Array();
-    const Max = 301;
-    const Min = 1;
+    let Nodes = [];
+    const Max = 590;
+    const Min = 10;
     let x, y = 0;
 
     for (let i = 0; i < nCities; i++) {
-        Nodes.push(Coordinates);
+        x = Math.floor(Math.random() * (Max - Min) + Min)
+        y = Math.floor(Math.random() * (Max - Min) + Min)
+        Nodes.push({ X: 0, Y: 0 });
+        Nodes[i].X = x;
+        Nodes[i].Y = y;
+        //console.log(Nodes[i].X, Nodes[i].Y,)
     }
-
-    for (let i = 0; i < nCities; i++) {
-        x = Math.random() * (Max - Min) + Min
-        y = Math.random() * (Max - Min) + Min
-
-        if (i < 5) {
-            Nodes[i].X = x;
-            Nodes[i].Y = y;
-            console.log(Nodes[i].X, Nodes[i].Y);
-
-        }
-    }
-
-Nodes.forEach(nodo =>{console.log(nodo.X, nodo.Y)})
-
+    Nodes[nCities] = Nodes[0];
+    console.log(Nodes);
     return Nodes;
 }
